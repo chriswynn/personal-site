6977c513dad4d2d7ce47333f3c5add2ce7f22a17
chriswynn <christopherwynndesign@gmail.com>
2017-10-26 15:04:05 -0400

    Added check for hero title in animation

--- assets/js/modules/heroAnimation.js
+++ assets/js/modules/heroAnimation.js
@@ -1,10 +1,12 @@
 const heroTitle = document.querySelector('.hero h1')
 
 export default function(scrollPosition) {
-  heroTitle.style.transform = `scale(${ 1 - (scrollPosition * .001)})`
-  if(scrollPosition > 920) {
-    heroTitle.style.opacity = 0
-  } else {
-    heroTitle.style.opacity = 1
+  if(heroTitle) {
+    heroTitle.style.transform = `scale(${ 1 - (scrollPosition * .001)})`
+    if(scrollPosition > 920) {
+      heroTitle.style.opacity = 0
+    } else {
+      heroTitle.style.opacity = 1
+    }
   }
 }
