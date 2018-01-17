2a83409a2dd1faedd2f7f885a571bc64987e07aa
chriswynn <christopherwynndesign@gmail.com>
2018-01-16 20:15:54 -0500

    Add dato cms to project

--- app.js
+++ app.js
@@ -5,13 +5,37 @@ const pageId = require('spike-page-id')
 const sugarml = require('sugarml')
 const sugarss = require('sugarss')
 const env = process.env.NODE_ENV
+const SpikeDatoCMS = require('spike-datocms')
+const locals = {}
+
+require('dotenv').config()
 
 module.exports = {
   devtool: 'source-map',
   matchers: { html: '*(**/)*.html', css: '*(**/)*.sss' },
   ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
+  plugins: [
+    new SpikeDatoCMS({
+      addDataTo: locals,
+      token: `${process.env.API_KEY}`,
+      models: [{
+        name: 'project',
+        template: {
+          path: 'views/project.html',
+          output: (project) => {
+            return `./${project.slug}.html`
+          }
+        }
+      }]
+    })
+  ],
   reshape: htmlStandards({
-    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
+    locals: (ctx) => {
+      return Object.assign(
+        locals,
+        {pageId: pageId(ctx)}
+      )
+    },
     minify: env === 'production'
   }),
   postcss: cssStandards({
